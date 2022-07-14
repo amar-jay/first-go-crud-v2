e021d954891090f44f839e4b0b1514e9e604d611
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2022-07-14 18:51:22 +0300

    changed orms

--- packages/config/app.go
+++ packages/config/app.go
@@ -11,7 +11,8 @@ var db *gorm.DB
 
 
 func Connection() {
-  dbInst, err := gorm.Open("postgres", "postgres:Abdel-manan1978@/gomovieapi?charset=utf8&parseTime=True&loc=Local",)
+  config := gorm.Config{}
+  dbInst, err := gorm.Open("postgres", postgres.Open("host=localhost user=manan password=manan dbname=gomoviesapi port=5432 sslmode=disable", &config)
   if err != nil {
     panic(err)
   }

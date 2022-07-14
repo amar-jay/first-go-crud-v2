d679c7034e2622851437a97dfba9a08edb8ef55c
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2022-07-14 18:01:58 +0300

    created

--- go.mod
+++ go.mod
@@ -11,6 +11,5 @@ require (
 require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.4 // indirect
-	github.com/lib/pq v1.10.2 // indirect
-	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	github.com/lib/pq v1.1.1 // indirect
 )
--- packages/config/app.go
+++ packages/config/app.go
@@ -1,16 +1,17 @@
 package config
 
 import (
-  "github.com/jinzhu/gorm"
-  _ "github.com/jinzhu/gorm/dialects/postgres"
-) 
+  "gorm.io/gorm"
+  "gorm.io/driver/postgres"
+)
+
 
 
 var db *gorm.DB
 
 
 func Connection() {
-  dbInst, err := gorm.Open("postgres", "manan:@/gomovieapi?charset=utf8&parseTime=True&loc=Local",)
+  dbInst, err := gorm.Open("postgres", "postgres:Abdel-manan1978@/gomovieapi?charset=utf8&parseTime=True&loc=Local",)
   if err != nil {
     panic(err)
   }
